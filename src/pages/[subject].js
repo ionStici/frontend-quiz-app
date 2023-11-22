cbc7395ae2c7589b101ff84e9dd3fe53e320fcc8
Ion Stici <ionsticidev@gmail.com>
2023-11-22 14:39:14 +0200

    Add subject component

--- src/pages/[subject].js
+++ src/pages/[subject].js
@@ -1,17 +1,47 @@
 import Header from "@/components/Header";
 import { getQuizzes } from "@/lib/getQuizzes";
 import styles from "./../styles/Subject.module.scss";
+import { useState } from "react";
 
 function SubjectPage({ quiz }) {
   const { questions } = quiz;
-  console.log(questions);
+
+  const [current, setCurrent] = useState(questions[0]);
+  const number = questions.indexOf(current) + 1;
+
+  function handleSubmit() {
+    setCurrent(questions[1]);
+  }
 
   return (
     <>
       <Header title={quiz.title} icon={quiz.icon} color={quiz.color} />
 
-      <section>
-        <p>{quiz.title}</p>
+      <section className={styles.section}>
+        <div className={styles.wrapper}>
+          <p className={styles.text}>Question {number} of 10</p>
+          <p className={styles.question}>{current.question}</p>
+          <div className={styles.bar}></div>
+
+          <div className={styles.quizzes}>
+            <ul>
+              {current.options.map((option, i) => {
+                const l = ["A", "B", "C", "D"];
+                return (
+                  <li key={i}>
+                    <button className={styles.answer_btn}>
+                      <span>{l[i]}</span>
+                      <span>{option}</span>
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+            <button className={styles.submit_btn} onClick={handleSubmit}>
+              Submit Answer
+            </button>
+          </div>
+        </div>
       </section>
     </>
   );
