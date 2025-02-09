6ddbc109b82857d2566be5e6cb77bcddace2d59c
Ion Stici <ionsticidev@gmail.com>
2025-02-09 08:03:11 +0200

    Add gh-pages 404 redirection

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path, { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/frontend-quiz-app",
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        notFound: resolve(__dirname, "404.html"),
+      },
+    },
+  },
 });
